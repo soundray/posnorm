5dcfb7b891e5051a4ef009c74a6400b0def1cbd6
Rolf Heckemann <git@soundray.de>
2018-04-09 09:24:01 +0000

    Use rigid MNI prealignment instead of CoG centering

--- posnorm.sh
+++ posnorm.sh
@@ -48,15 +48,15 @@ flipreg () {
     input=$1 ; shift
     output=$1 ; shift
     reflect-image $input reflected.nii.gz -x
-    register reflected.nii.gz $input -model Rigid -dofout rreg-input-reflected.dof.gz
+    register reflected.nii.gz $input -model Rigid -bg 0 -par "Final level" 1 -dofout rreg-input-reflected.dof.gz 
     bisect-dof rreg-input-reflected.dof.gz $output
 }
 
 midplane () {
     ltr=$1 ; shift
     lout=$1 ; shift
-    seg_maths $ltr -add 1 tr
-    read minx maxx miny maxy minz maxz <<< $(seg_stats tr -B)
+    seg_maths $ltr -add 1 tr.nii.gz
+    read minx maxx miny maxy minz maxz <<< $(seg_stats tr.nii.gz -B)
     n=$[$maxx/2]
     extract-image-region $ltr $lout -Rx1 $n -Rx2 $n -Ry1 $miny -Ry2 $maxy -Rz1 $minz -Rz2 $maxz # -Rt1 $mint -Rt2 $maxt
 }
@@ -71,6 +71,7 @@ do
         -mask)             mask=$(normalpath "$2"); shift;;
         -dof)            outdof=$(normalpath "$2"); shift;;
         -msp)               msp=$(normalpath "$2"); shift;;
+        -aligned)       aligned=$(normalpath "$2"); shift;;
         -debug)           debug=1 ;;
         --) shift; break;;
         -*)
@@ -95,24 +96,36 @@ test -e $img || fatal "posnorm input file does not exist"
 
 cd $td
 
-calculate-element-wise $img -mask $mask 0 -pad 0 -o masked.nii.gz
+cp $img image.nii.gz
+calculate-element-wise image.nii.gz -mask $mask 0 -pad 0 -o masked.nii.gz
 
-# Move CoG to centre of the grid 
-center masked.nii.gz prepped.nii.gz center1.dof.gz
+cp $cdir/MNI152_T1_1mm.nii.gz .
+cp $cdir/init-scale.dof.gz .
+register MNI152_T1_1mm.nii.gz masked.nii.gz -model Affine -dofin init-scale.dof.gz -par "Final level" 1 -dofout pre-affine.dof.gz >pre.log
+convert-dof pre-affine.dof.gz pre.dof.gz -output-format rigid
+transform-image masked.nii.gz prepped1.nii.gz -target MNI152_T1_1mm.nii.gz -dofin pre.dof.gz -interp "Fast linear with padding"
+seg_maths prepped1.nii.gz -otsu -mul prepped1.nii.gz prepped.nii.gz 
 
 # Subsample
-resample-image prepped.nii.gz resampled.nii.gz -size 2 2 2 -interp "Fast cubic bspline with padding" -padding 0
-smooth-image resampled.nii.gz blurred.nii.gz 3
+resample-image prepped.nii.gz resampled.nii.gz -padding 0 -size 2 2 2 -interp "Fast cubic bspline with padding" 
+#smooth-image resampled.nii.gz blurred.nii.gz 3
 
 # Estimate the linear transformation that aligns the MSP with the grid central sagittal plane
-flipreg blurred.nii.gz mspalign.dof.gz > flipreg.log
+#flipreg blurred.nii.gz mspalign.dof.gz > flipreg.log
+#flipreg prepped.nii.gz mspalign.dof.gz > flipreg.log
+flipreg resampled.nii.gz mspalign.dof.gz > flipreg.log
 
-compose-dofs center1.dof.gz mspalign.dof.gz $outdof
+compose-dofs pre.dof.gz mspalign.dof.gz $outdof
 
 if [[ ! -z $msp ]] ; then
-    transform-image $img aligned.nii.gz -dofin mspalign.dof.gz -interp "Fast linear with padding" -padding 0
+    transform-image $img aligned.nii.gz -dofin mspalign.dof.gz -interp "Fast linear with padding"
     midplane aligned.nii.gz msp.nii.gz
     cp msp.nii.gz $msp
+fi
+
+if [[ ! -z $aligned ]] ; then
+    test -e aligned.nii.gz || transform-image $img aligned.nii.gz -dofin mspalign.dof.gz -interp "Fast linear with padding"
+    cp aligned.nii.gz $aligned
 fi
 
 if [[ $debug ]]
@@ -122,3 +135,9 @@ then
 fi
 
 exit 0
+
+## Note: when transforming, part of an image can be rotated/shifted out of the grid. Hence -target MNI.
+
+## Todo: accuracy
+## efficiency
+## 
