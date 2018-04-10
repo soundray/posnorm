20ebadb3eb789f08abbc854d2a9ae2383877c7db
Rolf Heckemann <git@soundray.de>
2018-04-10 07:46:19 +0000

    Refactor midplane.sh

--- midplane.sh
+++ midplane.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+usage () {
+    msg "
+
+    Usage: $pn -img 3d-image.nii.gz -dofin mspalign.dof.gz -out mid-sagittal-plane.nii.gz]
+    
+    "
+}
+
 cdir=$(dirname $0)
 . $cdir/common
 cdir=$(normalpath $cdir)
@@ -10,20 +18,17 @@ td=$(tempdir)
 #trap 'cp -a $td $cdir' 0 1 2 3 13 15
 trap 'rm -r $td' 0 1 2 3 13 15
 
-export PATH=~/software/mirtk/build/lib/tools:$PATH
-
-usage () {
-    msg "
-
-    Usage: $pn -img 3d-image.nii.gz -dofin mspalign.dof.gz -out mid-sagittal-plane.nii.gz]
-    
-    "
-}
+which help-rst >/dev/null || fatal "MIRTK not on $PATH"
 
 . $cdir/midplane-function.sh
 
 [[ $# -eq 0 ]] && fatal "Parameter error" 
     
+img=
+dof=
+msp=
+debug=0
+label=
 while [[ $# -gt 0 ]]
 do
     case "$1" in
@@ -31,7 +36,7 @@ do
         -dofin)             dof=$(normalpath "$2"); shift;;
         -out)               msp=$(normalpath "$2"); shift;;
         -debug)           debug=1 ;;
-	-label)           label=1 ;;
+	-label)           label="-labels" ;;
         --) shift; break;;
         -*)
             fatal "Parameter error" ;;
@@ -43,20 +48,13 @@ done
 test -e $img || fatal "Input file does not exist"
 
 cd $td
-set -vx
-if [[ $label ]] ; then
-    transform-image $img aligned.nii.gz -labels -padding 0 -dofin $dof -interp "Fast linear with padding"
-else
-    transform-image $img aligned.nii.gz -padding 0 -dofin $dof -interp "Fast linear with padding"
-fi
 
+transform-image $img aligned.nii.gz $label -dofin $dof -interp "Fast cubic bspline with padding"
 midplane aligned.nii.gz msp.nii.gz
 cp msp.nii.gz $msp
 
-if [[ $debug ]]
-then
-    cd -
-    cp -a $td .
-fi
+[[ $debug -eq 1 ]] || exit 0
 
+cd -
+cp -a $td .
 exit 0
