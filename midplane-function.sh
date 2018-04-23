6c3fa1e7123678887097762a988d5a34f6c62bf0
Rolf Heckemann <git@soundray.de>
2018-04-23 16:33:37 +0200

    Quote variables in midplane-function.sh

--- midplane-function.sh
+++ midplane-function.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 midplane () {
-    ltr=$1 ; shift
-    lout=$1 ; shift
-    seg_maths $ltr -add 1 tr
+    ltr="$1" ; shift
+    lout="$1" ; shift
+    seg_maths "$ltr" -add 1 tr
     read minx maxx miny maxy minz maxz <<< $(seg_stats tr -B)
     n=$[$maxx/2]
-    extract-image-region $ltr $lout -Rx1 $n -Rx2 $n -Ry1 $miny -Ry2 $maxy -Rz1 $minz -Rz2 $maxz # -Rt1 $mint -Rt2 $maxt
+    extract-image-region "$ltr" "$lout" -Rx1 $n -Rx2 $n -Ry1 $miny -Ry2 $maxy -Rz1 $minz -Rz2 $maxz # -Rt1 $mint -Rt2 $maxt
 }
 
