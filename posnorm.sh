b6fa19e87592deaed1b6f3a0c6b83782fd1959e5
Rolf Heckemann <git@soundray.de>
2018-05-17 16:21:03 +0000

    Refactor posnorm.sh.  Minimize transformations by setting up a target space in flipreg, combining all dofs within flipreg, and transforming only once in preparation for flipped registration.  The preliminary transformation is now built from the output of the centering (with -cog) or MNI-aligning.  Translations are extracted and combined with downscaling.  This ensures that no part of the image gets cut off from being moved outside the target grid.
    Change interpolation on applying the preliminary transformation to
    sinc.  This increases both accuracy and runtime.  It also makes
    posnorm.sh (even) more specific to MR images.  At some point, the
    interpolation mode will be made selectable.
    
    Quantitative results are substantially better now.  Most likely this
    is mainly due to the earlier bug fix that eliminated duplicate
    application of the centering transform.
    
    A future addition may be a testing mode.  For now, the recommended
    testing procedure is to generate an image with perfect symmetry
    (e.g. by flipping an image and summation of the original with the
    flipped).  Then perturb with a random transformation.  Run posnorm.sh
    on the output and compare the aligned image with the non-perturbed
    image, and the recovered transformation with the inverse of the
    perturbing transformation.

--- posnorm.sh
+++ posnorm.sh
@@ -22,14 +22,24 @@ usage () {
 
 flipreg () {
     input="$1" ; shift
+    imgref="$1" ; shift
+    predof="$1" ; shift
     output="$1" ; shift
-    seg_maths $input -otsu -mul $input prepped.nii.gz 
-    # Subsample
-    resample-image prepped.nii.gz resampled.nii.gz -padding 0 -size 2 2 2 -interp "Fast cubic bspline with padding" 
-    smooth-image resampled.nii.gz blurred.nii.gz 3
-    reflect-image "$input" reflected.nii.gz -x
-    register reflected.nii.gz "$input" -model Rigid -bg 0 -par "Final level" 2 -dofout rreg-input-reflected.dof.gz 
-    bisect-dof rreg-input-reflected.dof.gz "$output"
+    seg_maths $input -otsu -mul $input premasked.nii.gz 
+    smooth-image premasked.nii.gz blurred.nii.gz 3
+    # Get translation from predof and round to voxel units
+    read tri trj trk < <( $info $predof | grep ^tx | tr -s ' ' | cut -d ' ' -f 3,6,9 )
+    read dimi dimk dimj < <( $info $imgref | grep Voxel.dimensions | tr -s ' ' | cut -d ' ' -f 4-6 )
+    roundi=$( echo 'round(' $tri',' $dimi ')' | $cdir/wrap.bc )
+    roundj=$( echo 'round(' $trj',' $dimj ')' | $cdir/wrap.bc )
+    roundk=$( echo 'round(' $trk',' $dimk ')' | $cdir/wrap.bc )
+    # Generate new predof from translation and downscaling
+    init-dof pre.dof.gz -tx $roundi -ty $roundj -tz $roundk -sx 200 -sy 200 -sz 200
+    # Create subsampled image space
+    transform-image blurred.nii.gz resampled.nii.gz -Sp 0 -target $imgref -dofin pre.dof.gz -interp "Sinc with padding"
+    reflect-image resampled.nii.gz reflected.nii.gz -x
+    register reflected.nii.gz resampled.nii.gz -model Rigid -bg 0 -par "Final level" 2 -dofout rreg-resampled-reflected.dof.gz 
+    bisect-dof rreg-resampled-reflected.dof.gz "$output"
 }
 
 cdir=$(dirname "$0")
@@ -118,16 +128,16 @@ then
 	cp "$cdir"/neutral.dof.gz prepre.dof.gz
     fi
     register ref.nii.gz masked.nii.gz -bg 0 -model Affine -dofin prepre.dof.gz -par "Final level" 2 -dofout pre-affine.dof.gz >pre.log 2>&1
-    convert-dof pre-affine.dof.gz pre.dof.gz -output-format rigid
-    transform-image masked.nii.gz prepped1.nii.gz -target ref.nii.gz -dofin pre.dof.gz -interp "Fast cubic bspline with padding"
+    convert-dof pre-affine.dof.gz pre-rigid.dof.gz -output-format rigid
+    # Estimate the rigid transformation that aligns the MSP with the grid central sagittal plane
+    flipreg masked.nii.gz ref.nii.gz pre-rigid.dof.gz mspalign.dof.gz > flipreg.log
 else
     # Estimate based on centre of gravity
     [[ $cog -eq 1 ]] || fatal "Use -cog option or supply reference image with -ref"
     centre masked.nii.gz prepped1.nii.gz pre.dof.gz
+    # Estimate the rigid transformation that aligns the MSP with the grid central sagittal plane
+    flipreg masked.nii.gz masked.nii.gz pre.dof.gz mspalign.dof.gz > flipreg.log
 fi
-
-# Estimate the rigid transformation that aligns the MSP with the grid central sagittal plane
-flipreg prepped1.nii.gz mspalign.dof.gz > flipreg.log
 
 compose-dofs pre.dof.gz mspalign.dof.gz "$outdof"
 
