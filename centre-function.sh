182409bd699451555bcb29e41bab49c53b4184c8
Rolf Heckemann <git@soundray.de>
2019-11-20 17:41:50 +0000

    Work around misfeature of seg_stats (writes warning message to stdout)

--- centre-function.sh
+++ centre-function.sh
@@ -1,7 +1,7 @@
 centre () {
     local f="$1" ; shift
     local dofout="$1" ; shift
-    read tx ty tz <<< $( seg_stats $f -c | cut -d ' ' -f 4-6 )
+    read tx ty tz <<< $( seg_stats $f -c | tail -n 1 | cut -d ' ' -f 4-6 )
     init-dof "$dofout" -tx $tx -ty $ty -tz $tz
     return
 }
